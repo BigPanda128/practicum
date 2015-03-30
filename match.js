a607de4b232f8aeae2bab387b8a59a0248293d42
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-30 12:59:16 -0400

    edited comments

--- match.js
+++ match.js
@@ -1,3 +1,9 @@
+/* Ryan Postma
+ * CMP237
+ * exam 2
+ * problem 2 in modify code
+ */
+
 var input = "A string with 7 numbers in it... 42 and 88, 123 and 16, 4 and 22.";
 var number = /\b(\d\d)\b/g;//modified so that it only grabs 2 digits
 var match;
