e91e085e9cfa7561c30e97e3b9ddc1191943e135
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-30 13:00:11 -0400

    edited coments

--- reverseNames.js
+++ reverseNames.js
@@ -1,3 +1,10 @@
+/*Ryan Postma
+ * CMP237
+ * exam 2
+ * problem 4 modify code section
+ * exra credit
+ */
+
 //edited reg exp so that it takes middle initial
 print(
   "Hopper, Grace M.\nMcCarthy, John\nRitchie, Dennis"
