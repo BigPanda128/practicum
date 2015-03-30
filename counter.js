1f724941710386a4bab53f39105cb19007402727
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-03-30 12:45:32 -0400

    number 1 is completed so that if it is over 100 it prints zillions of

--- counter.js
+++ counter.js
@@ -1,8 +1,11 @@
-var stock = "1 lemon, 2 cabbages, and 101 eggs";
+var stock = "1 lemon, 2 cabbages, and 101 eggs, and 150 limes";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
   if (amount == 1) // only one left, remove the 's'
     unit = unit.slice(0, unit.length - 1);
+  if (amount >= 100) {// check for 100 or over
+	  amount = "zillions of ";// print for the check
+  }
   else if (amount == 0)
     amount = "no";
   return amount + " " + unit;
